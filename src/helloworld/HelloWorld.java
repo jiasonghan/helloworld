7eee4e1154440ad0f309e4934570efcf4e46f03a
Administrator <Administrator@192.168.3.110>
2018-08-02 09:48:41 +0800

    hello

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package helloworld;
+/**
+* @author 贾松函
+* @version 创建时间:2018年8月2日 上午9:37:22
+*/
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
